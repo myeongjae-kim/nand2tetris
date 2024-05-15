73a00f16278dd10f47b2c8f0183d87e52504d65f
김명재 Myeongjae Kim <dev@myeongjae.kim>
2024-05-16 00:39:09 +0900

    [#6] pass ArrayTest

--- projects/10/compiler/test/ArrayTest.int.test.ts
+++ projects/10/compiler/test/ArrayTest.int.test.ts
@@ -2,22 +2,33 @@ import { describe, expect, it } from 'vitest';
 import { jackAnalyzer } from '../src/jackAnalyzer';
 import { fileTestTemplate } from './testHelper';
 import { readFilePromise } from '../src/readFilePromise';
+import { compilationEngine } from '../src/compilationEngine';
 
 describe('ArrayTest', () => {
   it('should compile Main.jack', async () => {
     const jackPath = './test/res/ArrayTest/Main.jack';
     const tokenXmlPath = './test/res/ArrayTest/MainT.xml';
+    const xmlPath = './test/res/ArrayTest/Main.xml';
     const expectedTokenXmlPath = './test/res/expected/ArrayTest/MainT.xml';
+    const expectedXmlPath = './test/res/expected/ArrayTest/Main.xml';
 
     await fileTestTemplate(async () => {
       await jackAnalyzer(jackPath);
 
-      const [xml, expectedXml] = await Promise.all([
+      const [tokenXml, expectedTokenXml] = await Promise.all([
         readFilePromise(tokenXmlPath),
         readFilePromise(expectedTokenXmlPath),
       ]);
 
-      expect(xml).toBe(expectedXml.replace(/\r/g, '').trim());
+      expect(tokenXml).toBe(expectedTokenXml.replace(/\r/g, '').trim());
+
+      await compilationEngine(tokenXmlPath);
+      const [xml, expectedXml] = await Promise.all([
+        readFilePromise(xmlPath),
+        readFilePromise(expectedXmlPath),
+      ]);
+
+      expect(xml).toBe(expectedXml.trim().replace(/\r/g, ''));
     }, tokenXmlPath);
   });
 });
